795b6700c6eb648e3413ff0f7ac1c45893d651af
random-person-on-internet <vedlakkad05@gmail.com>
2024-08-05 11:26:57 +0530

    lecture 5

--- week1/genericWithWildcardArgument.java
+++ week1/genericWithWildcardArgument.java
@@ -1,22 +1,23 @@
-class Student <T extends Number>{
+class Student<T extends Number> {
     String name;
     T[] marks;
 
-    Student(T[] marks){
+    Student(T[] marks) {
         this.marks = marks;
     }
 
-    double total(){
+    double total() {
         double sum = 0.0;
-        for(int i=0; i<marks.length; i++){
-            sum+= marks[i].doubleValue();
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i].doubleValue();
         }
         return sum;
     }
 
-    boolean compareMarks(Student<?> otherMarks){
-        if(total() == otherMarks.total()){
+    boolean compareMarks(Student<?> otherMarks) {
+        if (total() == otherMarks.total()) {
             return true;
         }
+        return false;
     }
 }
