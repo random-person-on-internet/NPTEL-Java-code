690932ebfd07e05ccbe6a1a84950d17343b5663e
random-person-on-internet <vedlakkad05@gmail.com>
2024-08-05 11:02:21 +0530

    lecture 4

--- week1/staticGenericClass2.java
+++ week1/staticGenericClass2.java
@@ -1,11 +1,19 @@
-class GenericClass<T> {
-    T obj;
+class GenericClass<T, V> {
+    T obj1;
+    V obj2;
 
-    GenericClass(T obj) {
-        this.obj = obj;
+    GenericClass(T obj1, V obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+
+        System.out.println(obj1);
+        System.out.println(obj2);
     }
+}
 
-    public void print(T obj) {
-        System.out.println(obj);
+public class staticGenericClass2 {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        GenericClass<Integer, String> a = new GenericClass<Integer, String>(103, "Hello");
     }
 }
